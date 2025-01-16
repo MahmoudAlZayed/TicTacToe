73c11332a256ee3e351d6fa4139d2f98c6417ba4
”MahmoudAlZayed” <mz.mahmoudal@gmail.com>
2025-01-16 11:21:00 +0100

    initial commit

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 
-// export default defineConfig({
-//   plugins: [react()],
-// })
+export default defineConfig({
+  base: "/Tic-Tac-Toe/",
+  plugins: [react()],
+})
 
-export default defineConfig({ base: "/{Tic-Tac-Toe}", plugins: [react()], })
+// export default defineConfig({ base: "/{Tic-Tac-Toe}", plugins: [react()], })
